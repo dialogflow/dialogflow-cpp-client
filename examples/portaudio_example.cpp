5e78432d6233f4dd940e5b99628a2a32492db3b3
Dmitriy Kuragin <kuragin@speaktoit.com>
2015-12-08 17:40:37 +0600

    portaudio example implemented

--- examples/portaudio_example.cpp
+++ examples/portaudio_example.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 
+#include <apiai/AI.h>
 #include <apiai/query/VoiceRequest.h>
 
 #include <portaudio.h>
+
+using namespace ai::query::request;
+using namespace ai::query::response;
+
+typedef struct _RecordInfo {
+    PaTime start_record_time;
+    VoiceRecorder *recorder;
+} RecordInfo;
 
 static int MyPaStreamCallback(
         const void *input, void *output,
@@ -11,13 +20,23 @@ static int MyPaStreamCallback(
         PaStreamCallbackFlags statusFlags,
         void *userData )
 {
-    std::cout << "Hello!" << std::endl;
+    RecordInfo* info = (RecordInfo *)userData;
+    if (info->start_record_time < 0.0) {
+        info->start_record_time = timeInfo->currentTime;
+    }
+
+    info->recorder->write((const char *)input, frameCount *sizeof(short));
+
+    if (timeInfo->currentTime - info->start_record_time > 3) {
+        return paComplete;
+    }
 
     return paContinue;
 }
 
 int main(int argc, char *argv[]) {
     Pa_Initialize();
+    ai::AI::global_init();
 
     auto default_input_device = Pa_GetDefaultInputDevice();
     auto device_info = Pa_GetDeviceInfo(default_input_device);
@@ -36,27 +55,47 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
-    PaStream* stream = NULL;
+    auto credentials = ai::Credentials("ff98c090685f484caaffada53cdce7b3", "4c91a8e5-275f-4bf0-8f94-befa78ef92cd");
 
-    auto open_error =
-    Pa_OpenStream(&stream,
-                  &stream_parameters,
-                  NULL,
-                  16000.0,
-                  512,
-                  paNoFlag,
-                  MyPaStreamCallback,
-                  NULL);
+    auto params = Parameters("<session id unique for every user>");
 
-    auto start_error = Pa_StartStream(stream);
+    VoiceRequest request("en", credentials, params);
 
-    PaError err = paNoError;
+    request.setVoiceSource([stream_parameters](ai::query::request::VoiceRecorder *recorder){
+        PaStream* stream = NULL;
 
-    while( ( err = Pa_IsStreamActive( stream ) ) == 1 )
-    {
-        Pa_Sleep(1000);
-    }
+        RecordInfo info = {0};
 
+        info.start_record_time = -1;
+        info.recorder = recorder;
+
+        auto open_error =
+        Pa_OpenStream(&stream,
+                      &stream_parameters,
+                      NULL,
+                      16000.0,
+                      512,
+                      paNoFlag,
+                      MyPaStreamCallback,
+                      &info);
+
+        auto start_error = Pa_StartStream(stream);
+
+        PaError err = paNoError;
+
+        while( ( err = Pa_IsStreamActive( stream ) ) == 1 )
+        {
+            Pa_Sleep(1000);
+        }
+
+        delete recorder;
+    });
+
+    auto response = request.perform();
+
+    std::cout << response << std::endl;
+
+    ai::AI::global_clean();
     Pa_Terminate();
     return 0;
 }
