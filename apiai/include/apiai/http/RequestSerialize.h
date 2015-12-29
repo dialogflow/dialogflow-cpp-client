893206a18fd5722ff3cbe361c81af96aba03ccf4
Dmitriy Kuragin <kuragin@speaktoit.com>
2015-12-29 13:53:38 +0600

    Any

--- apiai/include/apiai/http/RequestSerialize.h
+++ apiai/include/apiai/http/RequestSerialize.h
@@ -15,18 +15,13 @@ namespace ai{
         virtual T perform() {
             try {
                 auto resp = this->performConnection();
-//                return this->serialize(resp);
                 return this->serialize("resp");
-            } catch (ai::InvalidArgumentException &e) {
-                std::cout << e.what() << std::endl;
-                throw;
             } catch(...) {
-                throw ai::InvalidArgumentException("My Exception");
+                throw;
             }
         }
     protected:
         RequestSerialize(std::string URL): RequestConnection(URL) {}
-//        virtual T serialize(const std::string& response) = 0;
         virtual T serialize(const std::string& response) = 0;
     };
 }
